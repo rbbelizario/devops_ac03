39c435b52f06f2f4987e9b341d023a912b146ca1
rbbelizario <rbbelizario@gmail.com>
2020-10-08 22:28:24 -0300

    Teste codigo

--- com/beliza/test/test_operacoes.py
+++ com/beliza/test/test_operacoes.py
@@ -3,9 +3,12 @@ class TestClass:
         x = "this"
         assert "h" in x
 
+class TestClass:
+    def test_two(self):
+        x = 4
+        assert "30" in x
 
-
-
-
-
-
+class TestClass:
+    def test_tree(self):
+        x = "welcome"
+        assert "w" in x
